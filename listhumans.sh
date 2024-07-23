1203569d6bd5fcb2c5de69e5f4123c21027bb35e
vatsal1991 <145925878+vatsal1991@users.noreply.github.com>
2024-07-23 07:02:07 +0100

    Update listhumans.sh
    setting alias to list human users on system

--- listhumans.sh
+++ listhumans.sh
@@ -5,3 +5,7 @@
 #It filters users based on their uid
 
 awk -F ':' '$3 >= 1000 && $3 < 64434 {print $1}' /etc/passwd
+
+
+#If you want to set an alias to this, you will need to escape awk variables
+## alias humans="awk -F ':' '\$3 >= 1000 && \$3 < 64434 {print \$1}' /etc/passwd"
